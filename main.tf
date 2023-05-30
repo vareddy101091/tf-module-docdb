8023d706a1e963dca08b89714814ea368d8c9c24
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-30 16:35:29 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -41,8 +41,8 @@ resource "aws_docdb_cluster" "main" {
   cluster_identifier              = "${var.name}-${var.env}"
   engine                          = "docdb"
   engine_version                  = var.engine_version
-  master_username                 = data.aws_ssm_parameter_db_user.value
-  master_password                 = data.aws_ssm_parameter_db_pass.value
+  master_username                 = data.aws_ssm_parameter.db_user.value
+  master_password                 = data.aws_ssm_parameter.db_pass.value
   backup_retention_period         = 5
   preferred_backup_window         = "07:00-09:00"
   skip_final_snapshot             = true
